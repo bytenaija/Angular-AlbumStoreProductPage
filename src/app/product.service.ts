ee6dbdf904af0c5379d73d466531f946236e501e
Everistus Olumese <everistusemail@gmail.com>
2019-11-04 03:07:00 +0100

    Added product component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import {Album} from 'app/album';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id: number)
+  getAlbum(id: number): Observable<Album>
   {
-    return this._http.get(this._albumUrl).map(response => response.json())
+    return this._http.get(this._albumUrl).map(response => <Album> response.json())
   }
 
 }
