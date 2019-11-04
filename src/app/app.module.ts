a799483152f89428965692f19885b7a6f332a360
Everistus Olumese <everistusemail@gmail.com>
2019-11-04 02:55:29 +0100

    Added product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from 'app/app.component';
-import { ProductDescriptionComponent } from 'app/product-description/product-description.component';
-import { ProductPageComponent } from 'app/product-page/product-page.component';
-import { ProductService } from 'app/product.service';
+import { AppComponent } from './app.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductService } from './product.service';
 
 
 
